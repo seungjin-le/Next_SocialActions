ca8b1ab0aa6b75069527bcca9615b9ee62ca79a5
Seungjin Lee <dltmdwls154@gmail.com>
2023-05-26 16:19:45 +0900

    feat: Naver Login 구현

--- src/components/logins/kakaoLogin.tsx
+++ src/components/logins/kakaoLogin.tsx
@@ -3,17 +3,16 @@ import React, { useEffect } from "react";
 import { signIn, signOut, useSession } from "next-auth/react";
 
 const KakaoLogin = () => {
-  const {
-    data: { user },
-  } = useSession();
-  console.log(user);
+  const { data } = useSession();
+
   useEffect(() => {
-    console.log(user);
-  }, [user]);
+    console.log(data);
+  }, [data]);
 
   return (
     <div>
-      <button onClick={() => signIn("kakao")}>Sign in</button>
+      <button onClick={() => signIn("kakao")}>Kakao Sign in</button>
+      <button onClick={() => signIn("naver")}>Naver Sign in</button>
       <button onClick={() => signOut()}>Sign out</button>
     </div>
   );
--- src/pages/api/auth/[...nextauth].tsx
+++ src/pages/api/auth/[...nextauth].tsx
@@ -1,12 +1,17 @@
 import NextAuth from "next-auth";
 import KakaoProvider from "next-auth/providers/kakao";
 import type { NextAuthOptions } from "next-auth";
+import Naver from "next-auth/providers/naver";
 
 export const authOptions: NextAuthOptions = {
   providers: [
     KakaoProvider({
       clientId: process.env.KAKAO_LOGIN_API_KEY!,
       clientSecret: process.env.KAKAO_CLIENT_SECRET!,
+    }),
+    Naver({
+      clientId: process.env.NAVER_LOGIN_API_KEY!,
+      clientSecret: process.env.NAVER_CLIENT_SECRET!,
     }),
   ],
 };
